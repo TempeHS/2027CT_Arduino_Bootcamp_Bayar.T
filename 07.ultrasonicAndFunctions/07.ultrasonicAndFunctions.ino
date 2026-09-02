a64cd6fe3ba712c895750ee48ebdddb7c92decfe
Bayar T <bayar.tsogtulga@education.nsw.gov.au>
2026-09-02 03:50:15 +0000

    FEATURE:07.ultrasonicAndFunctions/07.ultrasonicAndFunctions.ino

--- 07.ultrasonicAndFunctions/07.ultrasonicAndFunctions.ino
+++ 07.ultrasonicAndFunctions/07.ultrasonicAndFunctions.ino
@@ -37,15 +37,30 @@
 
 #include "Ultrasonic.h"
 
-Ultrasonic ultrasonic(2);  // Grove 3-pin ultrasonic on D2 (single signal pin)
-
-const int BUZZER_PIN = 5;  // Grove Buzzer on D5
-const int LED_PIN = 6;     // Grove LED on D6
+Ultrasonic ultrasonic(2);   // Grove 3-pin sensor: trigger and echo share D2
 
 void setup() {
+  Serial.begin(115200);
+}
 
+int readDistance() {
+  return ultrasonic.read();
+}
+
+int classifyZone(int distance, int nearLimit, int farLimit) {
+  if (distance < nearLimit) {
+    return 0;              // danger
+  } else if (distance < farLimit) {
+    return 1;              // warning
+  }
+  return 2;                // safe
 }
 
 void loop() {
-
+  int distance = readDistance();
+  int zone = classifyZone(distance, 10, 30);
+  Serial.print(distance);
+  Serial.print(" cm, zone ");
+  Serial.println(zone);
+  delay(100);
 }
